43de4e3b4b80385308e16c81d14eb8db04e67e0d
luthfibg <luthfinewzoneonline@gmail.com>
2026-01-19 21:43:42 +0700

    fix api conn

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://backend.test'],
+    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://backend.test', 'http://192.168.1.10'],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
